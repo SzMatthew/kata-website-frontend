0f96ffbf69e858b33afce7c77711dec616ff4000
SzMatthew <szabomati@gmail.com>
2022-02-22 14:59:51 +0100

    Add navigation active class

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import './Navigation.scss';
 
 const Navigation = () => {
+  let location = useLocation();
+
   return (
     <nav className='navigation-container'>
       <ul className='navigation'>
-        <li className='item'>
+        <li className={location.pathname === '/' ? 'item active' : 'item'}>
           <Link to={`/`}>Kezdőlap</Link>
         </li>
-        <li className='item'>
+        <li className={location.pathname === '/konzultacio-es-arak' ? 'item active' : 'item'}>
           <Link to={`/konzultacio-es-arak`}>Konzultáció és árak</Link>
         </li>
-        <li className='item'>
+        <li className={location.pathname === '/rolam' ? 'item active' : 'item'}>
           <Link to={`/rolam`}>Rólam</Link>
         </li>
-        <li className='item'>
-          <Link to={`/folyamatrol`}>A folyamatról</Link>
+        <li className={location.pathname === '/folyamatrol' ? 'item active' : 'item'}>
+          <Link to={'/folyamatrol'}>A folyamatról</Link>
         </li>
-        <li className='item'>
+        <li className={location.pathname === '/velemenyek' ? 'item active' : 'item'}>
           <Link to={`/velemenyek`}>Vélemények</Link>
         </li>
-        <li className='item'>
+        <li className={location.pathname === '/kapcsolat' ? 'item active' : 'item'}>
           <Link to={`/kapcsolat`}>Kapcsolat</Link>
         </li>
       </ul>
