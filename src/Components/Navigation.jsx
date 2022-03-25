271b43569e92331e95e4f5f0ceccec8a79ae46d0
SzMatthew <szabomati@gmail.com>
2022-03-25 19:59:31 +0100

    Add mobile view of navigation

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,44 +1,46 @@
 import React, { useState, useEffect, useRef} from 'react';
 import { Link, useLocation } from "react-router-dom";
-import { GoThreeBars } from 'react-icons/go';
+import { GoThreeBars, GoX } from 'react-icons/go';
 import './SCSS/Navigation.scss';
 
 const Navigation = () => {
   let location = useLocation();
   const [isOpen, setIsOpen] = useState(false);
-  const [isMobileView, setIsMobileView ] = useState(false);
   const navigationRef = useRef(null);
 
   useEffect(() => {
-    setIsMobileView(window.getComputedStyle(navigationRef.current).getPropertyValue('flex-direction') === 'column');
-  }, []);
-
-  const handleOpen = () => {
-    if (isMobileView) {
-      setIsOpen(!isOpen);
+    if (isOpen) {
+      navigationRef.current.style.display = 'flex';
+    } else {
+      navigationRef.current.style.display = 'none';
     }
+  }, [isOpen]);
+
+  const handleOpen = () => {  
+    setIsOpen(!isOpen);
   };
 
   return (
     <nav className='navigation-container'>
       {
-        isMobileView && !isOpen
-        ? <GoThreeBars onClick={handleOpen} className='hamburger-icon'/>
-        : <ul ref={navigationRef} className='navigation'>
-            <li className={location.pathname === '/' ? 'item active' : 'item'}>
-              <Link to={`/`}>Kezdőlap</Link>
-            </li>
-            <li className={location.pathname === '/konzultacio-es-arak' ? 'item active' : 'item'}>
-              <Link to={`/konzultacio-es-arak`}>Konzultáció és árak</Link>
-            </li>
-            <li className={location.pathname === '/rolam' ? 'item active' : 'item'}>
-              <Link to={`/rolam`}>Rólam</Link>
-            </li>
-            <li className={location.pathname === '/kapcsolat' ? 'item active' : 'item'}>
-              <Link to={`/kapcsolat`}>Kapcsolat</Link>
-            </li>
-          </ul>
+        isOpen
+        ? <GoX onClick={handleOpen} className='hamburger-icon'/>
+        : <GoThreeBars onClick={handleOpen} className='hamburger-icon'/>
       }
+      <ul ref={navigationRef} className='navigation'>
+        <li className={location.pathname === '/' ? 'item active' : 'item'}>
+          <Link to={`/`}>Kezdőlap</Link>
+        </li>
+        <li className={location.pathname === '/konzultacio-es-arak' ? 'item active' : 'item'}>
+          <Link to={`/konzultacio-es-arak`}>Konzultáció és árak</Link>
+        </li>
+        <li className={location.pathname === '/rolam' ? 'item active' : 'item'}>
+          <Link to={`/rolam`}>Rólam</Link>
+        </li>
+        <li className={location.pathname === '/kapcsolat' ? 'item active' : 'item'}>
+          <Link to={`/kapcsolat`}>Kapcsolat</Link>
+        </li>
+      </ul>
     </nav>
     
   )
