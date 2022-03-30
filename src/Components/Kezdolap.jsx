ba1d8844b2c7632766bf6b4b87dbeb4c78d36e51
SzMatthew <szabomati@gmail.com>
2022-03-30 14:04:16 +0200

    Add basics of Kezdolap

--- src/Components/Kezdolap.jsx
+++ src/Components/Kezdolap.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './SCSS/Kezdolap.scss';
 
 const Kezdolap = () => {
   return (
-    <span>Kezdolap</span>
+    <div className='quote'>
+      <div className='quote__text'>"If you change the way you look at things, the things you look at change."</div>
+      <span className='quote__author'>- Wayne Dyer</span>
+    </div>
   )
 }
 
